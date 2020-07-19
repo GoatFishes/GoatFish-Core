b26c8f83c8e0a4debaa23e4e59a3a1d28d9f5980
LucasRodriguez <lucasrodriguezbenitez@gmail.com>
2020-07-20 00:24:01 +0100

    normalise testing

--- src/init.sh
+++ src/init.sh
@@ -7,5 +7,5 @@ echo > ./src/.env
 #getting current path
 CURRENT_PATH="$PWD"
 echo "CURRENT_PATH="$CURRENT_PATH > ./src/.env
-echo "EXCHANGESPORT=3003" >> ./src/.env
-echo "BOTSPORT=3002" >> ./src/.env
+echo "EXCHANGESPORT=3001" >> ./src/.env
+echo "BOTSPORT=3000" >> ./src/.env
